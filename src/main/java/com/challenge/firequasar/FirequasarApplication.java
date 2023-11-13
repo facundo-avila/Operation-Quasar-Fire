f095c8bcedb2dddb4ad11c061fbf7c805b7a6b3a
faku123faku <faku1234faku@gmail.com>
2023-11-12 23:00:16 -0300

    First commit

---
+++ src/main/java/com/challenge/firequasar/FirequasarApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.firequasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirequasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirequasarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/firequasar/FirequasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.firequasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirequasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
